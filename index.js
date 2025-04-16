416058ceb2e03b3c638309492821a741f0f24393
zagszy <yuvict1@outlook.com>
2025-04-16 15:59:47 -0700

    finished script

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const getGroups = async() => {
     await page.waitForSelector('.gptable');
 
     const groups = await page.evaluate(() => {
-        const tables = document.querySelectorAll('.gptable'); // selects all tables
-        let allRows = [];
+        const tables = document.querySelectorAll('.gptable'); // selects all tables and returns a node-list
+        let allRows = [];                                     // we'll use this to keep 1st column of rows 
 
         tables.forEach(table => {
-            const rows = Array.from(table.querySelectorAll('tr')).slice(1).map(row => {
-                const cells = row.querySelectorAll('td');
-                const nameLink = cells[0]?.querySelector('a'); 
-                const orderText = cells[2]?.textContent.trim();
-                const order = parseInt(orderText, 10);
-
+            const rows = Array.from(table.querySelectorAll('tr')).slice(1).map(row => { // does a few things:
+                const cells = row.querySelectorAll('td');                               // 1. table.querySelectorAll('tr) grabs row and subseq. cols
+                const nameLink = cells[0]?.querySelector('a');                          // 2. slice(1) grabs the data in table, slice(0) grabs header, not useful
+                const orderText = cells[2]?.textContent.trim();                         // 3. .map(row => ...) converts each 1st column into an array
+                const order = parseInt(orderText, 10);                                  // anyways, cells grabs all cells in the row, each row 6 columns, 
+                // we only want 0 for name + href, 2 order of group
                 return nameLink ? { 
                     name: nameLink.textContent.trim(), 
                     href: nameLink.href, 
